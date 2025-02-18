84fd357ed5d0394018eae030be035190457f2588
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2025-02-18 16:07:51 +0500

    tools style o'zgartirildi, notfound page o'zgaritrildi

--- src/pages/not_found/NotFoundPage.jsx
+++ src/pages/not_found/NotFoundPage.jsx
@@ -4,9 +4,10 @@ import "./not-found.css"
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>404</h1>
-      <p>Kecha uzr! Bunday sahifa topilmadi.</p>
-      <Link to="/">Bosh sahifaga qaytish</Link>
+      <h1>GAPLASHAMIZMI 😄</h1>
+      <h2>404</h2>
+      <p>Not found 😞 </p>
+      <Link to="/">Go Chat</Link>
     </div>
   );
 };
