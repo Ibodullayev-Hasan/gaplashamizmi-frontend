92183dbe85c6029bfef9c33d0ea5b79bb9f818ea
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-20 16:32:34 +0500

    fixed

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3015"; // API URL-ni o'zgartiring
+const SOCKET_URL = import.meta.env.SOCKET_URL; // API URL-ni o'zgartiring
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false, // Avtomatik ulanmasligi uchun
